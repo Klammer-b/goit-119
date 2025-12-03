c351c8d10d40817a2477dfa68f01deba25097ddb
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2025-12-03 20:49:26 +0200

    add validation

---
+++ src/validation/studentIdValidationSchema.js
@@ -0,0 +1,19 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+const validateObjectId = (value, helpers) => {
+  const isValid = isValidObjectId(value);
+
+  if (!isValid) {
+    return helpers.error('studentId.invalid');
+  }
+  return value;
+};
+
+export const studentIdValidationSchema = {
+  [Segments.PARAMS]: Joi.object({
+    studentId: Joi.string().required().custom(validateObjectId).messages({
+      'studentId.invalid': '"{#value}" must be a valid mongo id!',
+    }),
+  }),
+};
